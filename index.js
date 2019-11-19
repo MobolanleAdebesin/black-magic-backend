01c9817a35b939e1050627b3c099316d1640ecca
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-11-19 17:08:31 -0500

    Delete review function

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const Artist = require("./lib/models/Artist.js");
-const User = require("./lib/models/User.js");
-const Review = require("./lib/models/Review.js");
 const parser = require("body-parser");
 const artistRouter = require("./lib/routes/artists.js");
 const userRouter = require("./lib/routes/users.js");
+const reviewRouter = require("./lib/routes/reviews.js");
 app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
-app.get("/", function(req, res) {
-  Artist.find({}).then(artists => {
-    res.json(artists);
-  });
-});
-
-app.get("/reviews", function(req, res) {
-  Review.find({}).then(user => {
-    res.json(user);
-  });
-});
-
 app.use("/artists", artistRouter);
 app.use("/users", userRouter);
+app.use("/reviews", reviewRouter);
 
 app.set("port", process.env.PORT || 4000);
 
